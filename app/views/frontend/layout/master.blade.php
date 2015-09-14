cff8b6a3069c8adb970fb22e18f1177801251afa
sonnh123 <sonnh@kaopiz.com>
2015-09-14 13:29:14 +0700

    add table view for upload

--- app/views/frontend/layout/master.blade.php
+++ app/views/frontend/layout/master.blade.php
@@ -9,6 +9,9 @@
 	<script type="text/javascript" src="{{url('public/assets/js/jquery-1.11.3.min.js')}}"></script>
 	<script type="text/javascript" src="{{url('public/assets/js/masonry.pkgd.min.js')}}"></script>
 	<script src="{{url('public/assets/js/jquery.nicescroll.js')}}" type='text/javascript'></script>
+    {{ HTML::style('public/assets/css/jquery-ui.min.css') }}
+    {{ HTML::script('public/assets/js/jquery-ui.min.js') }}
+    {{ HTML::script('public/assets/js/scripts.js') }}
 </head>
 <body>
 	<header>
--- app/views/frontend/users/upload.blade.php
+++ app/views/frontend/users/upload.blade.php
@@ -6,19 +6,26 @@
 	Photo Upload
 @stop
 @section('content')
-	<div class="upload_content">
-		<div class="upload_left">
+  <script>
+  
+  </script>
+	<div id='upload-image-tabs' class="upload_content">
+          <ul>
+            <li><a href="#upload-image-tabs">Đăng Ảnh</a></li>
+            <li><a href="#upload-album-tabs">Đăng album</a></li>
+            
+          </ul>
+		<div id='upload-image-tabs' class="upload_left">
 			<h1>Đăng ảnh</h1>
 			<p class="note">Sử dụng nút <span>Chọn ảnh</span> để chọn ảnh cho album của bạn. Đăng <span>tẹt ga thoải con gà mái nhé</span>!</p>
 			<ul>
 				<li>
 					<p>Thể loại</p>
 					<select>
-						<option>Lựa chọn chuyên mục</option>
-						<option>Phong cảnh</option>
-						<option>Sexy</option>
-						<option>Động vật</option>
-						<option>Chó mèo</option>
+                        <option></option>
+                        @foreach($categories as $index => $category)
+                        <option>{{$category->title}}</option>
+                        @endforeach
 					</select>
 				</li>
 				<li>
@@ -31,7 +38,7 @@
 				</li>
 			</ul>
 		</div>
-		<div class="upload_right">
+		<div id='upload-album-tabs' class="upload_right">
 			
 		</div>
 	</div>
