dfa1505801e55f8ab852048d8511a1e8f5493463
sonnh123 <sonnh@kaopiz.com>
2015-09-26 12:14:38 +0700

    update composer

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -1946,7 +1946,6 @@ return array(
     'System_SSH_Agent_Identity' => $vendorDir . '/phpseclib/phpseclib/phpseclib/System/SSH/Agent.php',
     'TestCase' => $baseDir . '/app/tests/TestCase.php',
     'User' => $baseDir . '/app/models/User.php',
-    'Users' => $baseDir . '/app/models/Users.php',
     'UsersController' => $baseDir . '/app/controllers/UsersController.php',
     'Whoops\\Exception\\ErrorException' => $vendorDir . '/filp/whoops/src/Whoops/Exception/ErrorException.php',
     'Whoops\\Exception\\Formatter' => $vendorDir . '/filp/whoops/src/Whoops/Exception/Formatter.php',
