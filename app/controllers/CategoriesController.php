c6de4bca04b99eaf6f7a2ee144fb25791035ce7f
sonnh123 <sonnh@kaopiz.com>
2015-10-11 12:27:06 +0700

    add public when view category

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -2,7 +2,7 @@
 class CategoriesController extends BaseController{
     public function getView($id) {
         $data['category'] = Category::where('id', $id)->first();
-        $data['albums'] = Album::where('category_id', $id)->get();
+        $data['albums'] = Album::where('category_id', $id)->where('public', '=', 1)->get();
         return View::make('frontend/categories/view')->with('data', $data);
 
     }
