7a23ca893179512303743a7e1a34d0f1ad067c3b
sonnh123 <sonnh@kaopiz.com>
2015-12-10 22:52:55 +0700

    add check like many timtimes

--- public/assets/js/ajax-like.js
+++ public/assets/js/ajax-like.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
+    var count_click = 0;
+    window.setInterval(function(){count_click = 0}, 5000);
    $("i.like").click(function(){
-       var url = $(this).attr('itemref');
-       var count = parseInt($(this).next('span.count-like').text());
-       var obj = $(this);
-       $.ajax({
-          url:url,
-          type: 'PATCH',
-          data:{
-              _method: 'PATCH',
-              
-          }, 
-          success: function(result){
-              if(result == 'true'){
-                  count += 1;
-                  obj.next('span.count-like').text(count);
-              }
-          }
-       });
+       count_click ++;
+       if(count_click <= 3){
+            var url = $(this).attr('itemref');
+            var count = parseInt($(this).next('span.count-like').text());
+            var obj = $(this);
+            $.ajax({
+               url:url,
+               type: 'PATCH',
+               data:{
+                   _method: 'PATCH',
+
+               }, 
+               success: function(result){
+                   if(result == 'true'){
+                       count += 1;
+                       obj.next('span.count-like').text(count);
+                   }
+               }
+            });
+        }else{
+            alert('Like không hợp lệ');
+            count_click = 0;
+        }
    }) ;
 });
